3cb2b6cf0ed3b3f9b4de313bb55a3c4965e318c3
rxomidov <rixsitilloxomidov@gmail.com>
2024-06-08 18:43:09 +0500

    login added

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,30 @@
 <template>
-  <div class="about">
-    <h1>This is an login page</h1>
-  </div>
+  <div class="onm-login__page">
+  <div class="onm-login">
+    <div class="onm-login__wrapper d-flex justify-content-center">
+      <div class="onm-login__form--card">
+        <div class="onm-login__form--login">
+          <a href="/" class="router-link-active">
+            <img src="https://online-mahalla.uz/media/store/img/header-logo.svg" alt="logo">
+            </a>
+            </div> <div class="onm-login__form--header"><p>Хуш келибсиз</p> <h3>Тизимга кириш
+
+            </h3></div> <form class="onm-login__form"><div class="onm-login__form--item">
+              <label>Логин</label> <span><input type="text" placeholder="Киритиш"></span>
+              </div> <div class="onm-login__form--item"><label>Парол</label> <span>
+                <input placeholder="Парол" type="password"> <span class="onm-login__eye">
+                  <img src="https://online-mahalla.uz/media/store/img/hide-password.svg" alt="online-mahalla"></span></span>
+                  </div> <!----> <div class="onm-login__actions d-flex flex-column align-items-center">
+                    <button type="button" class="onm-btn onm-btn--blue onm-btn--r5 onm-btn--w100p d-flex align-items-center my-3">
+                      <span>Тизимга кириш</span> </button> 
+                      <button type="button" class="onm-btn onm-btn--dark-blue onm-btn--r5 onm-btn--w100p d-flex align-items-center"><span>ЭРИ билан кириш</span>
+                       </button></div></form></div> <div class="onm-login__banner d-md-flex align-items-center justify-content-center"><div class="onm-login__banner--logo">
+                        <img src="https://online-mahalla.uz/media/store/img/login-white.svg" alt="online-mahalla"></div>
+                         <div class="onm-login__links"><li><a href="https://t.me/onlinemahalla">
+                          <img src="@/assets/img/telegram_black.svg" alt="telegram"></a></li> 
+                          <li><a href="@">
+                            <img src="@/assets/img/instagram_black.svg" alt="instagram"></a></li>
+                            </div></div></div></div></div>
 </template>
 
 <style>
