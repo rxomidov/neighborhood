dc51f135bf59d3126cb72f489022aa341cb901a7
rxomidov <rixsitilloxomidov@gmail.com>
2024-08-06 18:26:22 +0500

    changes

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,21 +5,21 @@
       <div class="onm-login__form--card">
         <div class="onm-login__form--login">
           <a href="/" class="router-link-active">
-            <img src="https://online-mahalla.uz/media/store/img/header-logo.svg" alt="logo">
+            <img src="@/assets/img/header-logo.png" height="48px" alt="logo">
             </a>
-            </div> <div class="onm-login__form--header"><p>Хуш келибсиз</p> <h3>Тизимга кириш
+            </div> <div class="onm-login__form--header"><p>Xush kelibsiz</p> <h3>Tizimga kirish
 
             </h3></div> <form class="onm-login__form"><div class="onm-login__form--item">
-              <label>Логин</label> <span><input type="text" placeholder="Киритиш"></span>
-              </div> <div class="onm-login__form--item" style="position: relative;"><label>Парол</label> <span>
+              <label>Login</label> <span><input type="text" placeholder="Киритиш"></span>
+              </div> <div class="onm-login__form--item" style="position: relative;"><label>Parol</label> <span>
                 <input placeholder="Парол" type="password"> <span class="onm-login__eye">
                   <img src="https://online-mahalla.uz/media/store/img/hide-password.svg" alt="online-mahalla"></span></span>
                   </div> <!----> <div class="onm-login__actions d-flex flex-column align-items-center">
                     <button type="button" class="onm-btn onm-btn--blue onm-btn--r5 onm-btn--w100p d-flex align-items-center my-3">
-                      <span>Тизимга кириш</span> </button> 
-                      <button type="button" class="onm-btn onm-btn--dark-blue onm-btn--r5 onm-btn--w100p d-flex align-items-center"><span>ЭРИ билан кириш</span>
+                      <span>Tizimga kirish</span> </button> 
+                      <button type="button" class="onm-btn onm-btn--dark-blue onm-btn--r5 onm-btn--w100p d-flex align-items-center"><span>ERI bilan kirish</span>
                        </button></div></form></div> <div class="onm-login__banner d-md-flex align-items-center justify-content-center"><div class="onm-login__banner--logo">
-                        <img src="https://online-mahalla.uz/media/store/img/login-white.svg" alt="online-mahalla"></div>
+                        <img src="@/assets/img/header-logo.png" alt="mahalla"></div>
                          <div class="onm-login__links"><li><a href="https://t.me/onlinemahalla">
                           <img src="@/assets/img/telegram_black.svg" alt="telegram"></a></li> 
                           <li><a href="@">
